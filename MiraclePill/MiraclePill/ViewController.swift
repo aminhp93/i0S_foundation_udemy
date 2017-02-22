88ac584592976ae905642667b71b06e864a498d1
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-21 20:17:30 -0800

    45 - iOS 10 App: Miracle Pills 03 - UIPickerView

--- MiraclePill/MiraclePill/ViewController.swift
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var statePickerButton: UIButton!
+    
+    let states = ["Alaska", "California", "New York"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
         
     }
 
@@ -21,6 +29,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateButtonPressed(_ sender: UIButton) {
+        statePicker.isHidden = false
+    }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerButton.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
 }
 
