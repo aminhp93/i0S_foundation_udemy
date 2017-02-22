76d77b0c09acaf5ee64beb740be1f7fbd48279dd
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-21 18:50:06 -0800

    43 - iOS 10 App: Miracle Pills 01 - Autolayout & project creation

---
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Minh Pham on 2/21/17.
+//  Copyright © 2017 Minh Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
